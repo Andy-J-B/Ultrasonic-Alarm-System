2d7f249ac722fb186eb329dc8c25dfaab0975842
Andy-J-B <080.abae@gmail.com>
2025-09-20 20:41:10 -0700

    Add multiple led lights for each state

--- UltrasonicAlarmSystem.ino
+++ UltrasonicAlarmSystem.ino
@@ -4,25 +4,45 @@
 #define alarmPin 8
 #define USEcho 10
 #define USTrigger 9
-#define redLed1 2
-#define redLed2 7
+#define firstRedLed 2
+#define latchPin 11
+#define clockPin 6
+#define dataPin 12
+
 
 float timing = 0.0;
 float distance = 0.0;
+
+byte leds = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(alarmPin, OUTPUT);
   pinMode(USEcho, INPUT);
   pinMode(USTrigger, OUTPUT);
-  pinMode(redLed1, OUTPUT);
-  pinMode(redLed2, OUTPUT);
+  pinMode(firstRedLed, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+
 
   Serial.begin(9600);
   Serial.println("Starting the Ultrasonic Alarm System");
 }
 
+
+void updateShiftRegister()
+{
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, leds);
+  digitalWrite(latchPin, HIGH);
+}
+
+
 void loop() {
+  leds = 0;
+  digitalWrite(USTrigger, LOW);
+  delayMicroseconds(2);
   // put your main code here, to run repeatedly:
   digitalWrite(USTrigger, HIGH);
   delayMicroseconds(10);
@@ -39,16 +59,29 @@ void loop() {
   Serial.print(distance / 2.54);
   Serial.println("in");
 
- 
+
+
   if (distance <= 20) {
-    tone(alarmPin, 500);
-    digitalWrite(redLed1, HIGH);
-    digitalWrite(redLed2, HIGH);
+    // turn on middle 3 leds
+    digitalWrite(firstRedLed, HIGH);
+    leds = 3;
+    updateShiftRegister();
+    
+    tone(alarmPin, 250);
+  }  else if (distance <= 50) {
+    // turn on first 3 leds
+    digitalWrite(firstRedLed, LOW);
+    leds = 28;
+    updateShiftRegister();
+    tone(alarmPin, 150);
   } else {
+    digitalWrite(firstRedLed, LOW);
+    leds = 224;
+    updateShiftRegister();
+    // turn on last 3 leds
     noTone(alarmPin);
-    digitalWrite(redLed1, LOW);
-    digitalWrite(redLed2, LOW);
   }
+  Serial.println(leds);
 
   delay(100);
 }
