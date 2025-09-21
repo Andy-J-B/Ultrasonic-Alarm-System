5380d623a277657010d31382c69cc93209b9de8d
Andy-J-B <080.abae@gmail.com>
2025-09-21 12:46:17 -0700

    Add LCD1602 display distance and state of alarm system

--- UltrasonicAlarmSystem.ino
+++ UltrasonicAlarmSystem.ino
@@ -1,12 +1,15 @@
 // Author : Andy Bae
 
+// Import libraries
+
+#include <LiquidCrystal.h>
+
 // Set pins
 #define alarmPin 8
 #define USEcho 10
 #define USTrigger 9
-#define firstRedLed 2
 #define latchPin 11
-#define clockPin 6
+#define clockPin 13
 #define dataPin 12
 
 
@@ -15,12 +18,14 @@ float distance = 0.0;
 
 byte leds = 0;
 
+LiquidCrystal lcd(2,3,4,5,6,7);
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(alarmPin, OUTPUT);
   pinMode(USEcho, INPUT);
   pinMode(USTrigger, OUTPUT);
-  pinMode(firstRedLed, OUTPUT);
+
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
@@ -28,6 +33,9 @@ void setup() {
 
   Serial.begin(9600);
   Serial.println("Starting the Ultrasonic Alarm System");
+
+  // start lcd display
+  lcd.begin(16, 2);
 }
 
 
@@ -40,6 +48,7 @@ void updateShiftRegister()
 
 
 void loop() {
+  lcd.clear();
   leds = 0;
   digitalWrite(USTrigger, LOW);
   delayMicroseconds(2);
@@ -59,23 +68,41 @@ void loop() {
   Serial.print(distance / 2.54);
   Serial.println("in");
 
+  lcd.setCursor(0, 1);
 
 
+  if (distance < 100) {
+    lcd.print("Distance : ");
+
+    lcd.print(distance, 0);
+    lcd.print(" cm");
+  } else {
+    lcd.print("No Detections");
+
+  }
+  
+  
+
+
+  lcd.setCursor(0, 0);
   if (distance <= 20) {
     // turn on middle 3 leds
-    digitalWrite(firstRedLed, HIGH);
+    lcd.print("WARNING");
+
     leds = 3;
     updateShiftRegister();
     
     tone(alarmPin, 250);
   }  else if (distance <= 50) {
     // turn on first 3 leds
-    digitalWrite(firstRedLed, LOW);
+    lcd.print("DANGER");
+
     leds = 28;
     updateShiftRegister();
     tone(alarmPin, 150);
   } else {
-    digitalWrite(firstRedLed, LOW);
+    lcd.print("NO THREATS");
+
     leds = 224;
     updateShiftRegister();
     // turn on last 3 leds
@@ -83,5 +110,5 @@ void loop() {
   }
   Serial.println(leds);
 
-  delay(100);
+  delay(300);
 }
